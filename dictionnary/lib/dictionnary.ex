16045453a9d74af9fe5f09bb075a933472089776
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2018-08-20 10:42:50 +0200

    Refactor the code to use the |> operator

--- dictionnary/lib/dictionnary.ex
+++ dictionnary/lib/dictionnary.ex
@@ -1,23 +1,12 @@
 defmodule Dictionnary do
-  @moduledoc """
-  Documentation for Dictionnary.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionnary.hello()
-      :world
-
-  """
   def word_list do
-    contents = File.read!("assets/words.txt")
-    list = String.split(contents, ~r/\n/)
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
 
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 end

1b4dbdb24596cfb23f4c4059cc82be0abc13c904
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2018-08-21 12:48:28 +0200

    Add some unit tests for Hangman.Game.new_game()

---
+++ game/hangman/test/hangman/game_test.exs
@@ -0,0 +1,16 @@
+defmodule HangmanGameTest do
+  use ExUnit.Case
+  doctest Hangman.Game
+
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+    assert Enum.all?(game.letters, fn x -> x =~ ~r/^[a-z]$/ end)
+  end
+
+end

2c920a8cc803eba000f9c6bcc853136d916ac178
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2018-08-21 13:15:42 +0200

    Add Hangman.Game.make_move() function
    This function needs to check whether the guess has already been used or not.
    This cannot be done using pattern matching or a guard clause. So we will delegate
    the work to another function containing one extra argument, the fact that the
    guess is already in the used MapSet or not.
    Doing this avoids to write a conditional in our make_move() function.

--- game/hangman/lib/hangman/game.ex
+++ game/hangman/lib/hangman/game.ex
@@ -4,7 +4,7 @@ defmodule Hangman.Game do
     turns_left: 7,
     game_state: :initializing,
     letters: [],
-
+    used: MapSet.new(),
   ]
 
   def new_game() do
@@ -15,6 +15,19 @@ defmodule Hangman.Game do
 
   def make_move(game = %{ game_state: state }, _) when state in [:won, :lost] do
     { game, tally(game) }
+  end
+
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    { game, tally(game) }
+  end
+
+  defp accept_move(game, guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  defp accept_move(game, guess, _already_guessed = false) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
   end
 
   def tally(game) do
--- game/hangman/test/hangman/game_test.exs
+++ game/hangman/test/hangman/game_test.exs
@@ -20,4 +20,18 @@ defmodule HangmanGameTest do
     end
   end
 
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
 end

323779e31a6f98387b5ebd80038cb7c954664d01
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2018-08-21 14:37:07 +0200

    Add score_guess() implementation for bad guess

--- game/hangman/lib/hangman/game.ex
+++ game/hangman/lib/hangman/game.ex
@@ -42,8 +42,12 @@ defmodule Hangman.Game do
     struct(game, game_state: new_state)
   end
 
-  defp score_guess(game, _not_good_guess) do
-    game
+  defp score_guess(game = %{turns_left: 1}, _not_good_guess) do
+    struct(game, game_state: :lost, turns_left: 0)
+  end
+
+  defp score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
+    struct(game, game_state: :bad_guess, turns_left: turns_left - 1)
   end
 
   defp maybe_won(true), do: :won

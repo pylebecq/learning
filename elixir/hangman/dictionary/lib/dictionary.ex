5ca0c3e54daba7a79039604ce1b2c0bf144283b1
Pierre-Yves Lebecq <pylebecq@users.noreply.github.com>
2022-09-10 10:44:31 +0200

    [hangman] Implement Dictionary.random_word/1

--- elixir/hangman/dictionary/lib/dictionary.ex
+++ elixir/hangman/dictionary/lib/dictionary.ex
@@ -1,18 +1,11 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for `Dictionary`.
-  """
+  @word_list "assets/words.txt"
+             |> File.read!()
+             |> String.split("\n", trim: true)
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec random_word :: binary
+  def random_word do
+    @word_list
+    |> Enum.random()
   end
 end

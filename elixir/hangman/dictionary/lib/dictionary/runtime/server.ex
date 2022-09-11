6cb7e3e2df40d1deee72498a25460dfe44a581b6
Pierre-Yves Lebecq <pylebecq@users.noreply.github.com>
2022-09-11 14:02:33 +0200

    [hangman] Add supervision to the dictionary server

--- elixir/hangman/dictionary/lib/dictionary/runtime/server.ex
+++ elixir/hangman/dictionary/lib/dictionary/runtime/server.ex
@@ -1,15 +1,21 @@
 defmodule Dictionary.Runtime.Server do
   alias Dictionary.Impl.WordList
 
+  use Agent
+
   @type t :: pid
 
   @name __MODULE__
 
-  def start_link do
+  def start_link(_) do
     Agent.start_link(&WordList.word_list/0, name: @name)
   end
 
   def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(@name, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(@name, &WordList.random_word/1)
   end
 end

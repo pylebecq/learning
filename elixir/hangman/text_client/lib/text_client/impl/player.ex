3ad2de3f3a250289b4eb1ff44ee167f9722b3c85
Pierre-Yves Lebecq <pylebecq@users.noreply.github.com>
2022-09-11 19:26:49 +0200

    [hangman] Update text_client to recent changes to hangman API

--- elixir/hangman/text_client/lib/text_client/impl/player.ex
+++ elixir/hangman/text_client/lib/text_client/impl/player.ex
@@ -20,8 +20,8 @@ defmodule TextClient.Impl.Player do
     IO.puts(feedback_for(tally))
     IO.puts(current_word(tally))
 
-    Hangman.make_move(game, get_guess())
-    |> interact()
+    tally = Hangman.make_move(game, get_guess())
+    interact({game, tally})
   end
 
   defp feedback_for(tally = %{game_state: :initializing}) do

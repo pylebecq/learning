7c9d82e2ca9d71a7ef93ca6e79162e8ed57e64f5
Pierre-Yves Lebecq <pylebecq@users.noreply.github.com>
2022-09-11 15:30:34 +0200

    [hangman] Add GameServer to keep game state

---
+++ elixir/hangman/hangman/lib/hangman/runtime/game_server.ex
@@ -0,0 +1,27 @@
+defmodule Hangman.Runtime.GameServer do
+  alias Hangman.Impl.Game
+
+  use GenServer
+
+  @type t :: pid
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    state = Game.new_game()
+
+    {:ok, state}
+  end
+
+  def handle_call({:make_move, guess}, _from, state) do
+    {new_state, tally} = Game.make_move(state, guess)
+
+    {:reply, tally, new_state}
+  end
+
+  def handle_call({:tally}, _from, state) do
+    {:reply, Game.tally(state), state}
+  end
+end
